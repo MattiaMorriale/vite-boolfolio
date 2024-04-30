4fd6e3f7320f013722444a358f0884314d60ec14
mattia_morriale <mattia.morriale1@gmail.com>
2024-04-30 18:42:33 +0200

    mod

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -9,22 +9,6 @@ export default {
 </script>
 
 <template>
-    <!-- <li class="border border-light border-1 p-3 rounded-2 ">
-        <div class="img-cover rounded-3">
-            <img class="rounded-3" :src="project.image" alt="">
-        </div>
-        <h2 class="fs-4 mt-4 mb-2">{{ project.name }}</h2>
-        <ul class="d-flex gap-2 list-unstyled">
-            <li class="px-3 bg-white rounded-3 text-black mb-3" v-for="currentTech in project.technologies">{{
-                currentTech.type }}</li>
-        </ul>
-        <p class="mb-4">{{ project.description }}</p>
-
-        <span v-if="project.type" class="border border-1 border-white p-2 rounded-3"> {{ project.type.name
-            }}
-        </span>
-
-    </li> -->
 
     <li class="card" style="width: 18rem;">
         <img :src="project.image"  class="card-img-top" alt="@">
