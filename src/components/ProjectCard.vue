6cd10c408d5c2dfab7883fb8f0483727ca0664d5
mattia_morriale <mattia.morriale1@gmail.com>
2024-04-30 18:42:02 +0200

    mod card style

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -9,7 +9,7 @@ export default {
 </script>
 
 <template>
-    <li>
+    <!-- <li class="border border-light border-1 p-3 rounded-2 ">
         <div class="img-cover rounded-3">
             <img class="rounded-3" :src="project.image" alt="">
         </div>
@@ -24,15 +24,27 @@ export default {
             }}
         </span>
 
+    </li> -->
+
+    <li class="card" style="width: 18rem;">
+        <img :src="project.image"  class="card-img-top" alt="@">
+        <div class="card-body d-flex flex-column justify-content-between ">
+            <div>
+                <h5 class="card-title">{{ project.name }}</h5>
+                <ul class="d-flex gap-2 list-unstyled">
+                    <li class="px-3 bg-white rounded-3 text-black mb-3" v-for="currentTech in project.technologies">{{
+                    currentTech.type }}</li>
+                </ul>
+                <p class="card-text">{{ project.description }}</p>
+                <span v-if="project.type" class="d-block text-primary mb-3"> {{ project.type.name}}</span>
+            </div>
+            <div>
+                <a :href="project.link_GitHub" class="btn btn-primary">Git-Hub</a>
+            </div>
+        </div>
     </li>
 </template>
 
 <style lang="scss" scoped>
-li {
-    .img-cover {
-        img {
-            width: 100%;
-        }
-    }
-}
+
 </style>
