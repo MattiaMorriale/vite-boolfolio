b7487cb495a4ac3a3e6ac3e348796bc1dc5d6eff
vi_ser <vinceulisse@gmail.com>
2024-04-30 16:34:45 +0200

    creazione nav e main

---
+++ src/components/AppMain.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+    name: 'AppMain',
+
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<template>
+    <main>
+        <div class="container py-5">
+            <ul class="list-unstyled d-flex">
+                <li>
+                    <div class="img-cover">
+                        <img src="https://picsum.photos/400/500" alt="">
+                    </div>
+                    <span>Name</span>
+                    <p>Description</p>
+                </li>
+
+                <li>
+                    <div class="img-cover">
+                        <img src="https://picsum.photos/400/500" alt="">
+                    </div>
+                    <span>Name</span>
+                    <p>Description</p>
+                </li>
+
+                <li>
+                    <div class="img-cover">
+                        <img src="https://picsum.photos/400/500" alt="">
+                    </div>
+                    <span>Name</span>
+                    <p>Description</p>
+                </li>
+            </ul>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+    .container {
+        ul {
+            li {
+                width: calc(100% / 3);
+            }
+        }
+    }
+}
+</style>
