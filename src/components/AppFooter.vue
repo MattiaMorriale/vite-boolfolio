9b6aa13cf418511835d9490bcc179b7aa828f60d
mattia_morriale <mattia.morriale1@gmail.com>
2024-05-01 23:04:32 +0200

    add the footer and technologies logo

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,10 +1,13 @@
 <script>
 
+import { store } from '../store';
+
 export default {
     name: 'AppFooter',
 
     data() {
         return {
+            store,
         }
     },
 
@@ -13,20 +16,36 @@ export default {
 
 <template>
     <footer class="mb-0">
-        <div class="container py-3 d-flex align-items-center justify-content-between">
-            <div class="logo-container">
+        <div class="container py-3 mb-3 d-flex align-items-start justify-content-between">
+            <div class="col-4 pt-5 ">
                 <span class="fs-1">Tecnologie Utilizzate</span>
             </div>
-            <ul class="d-flex list-unstyled gap-5 mb-0 text-uppercase">
-                <li>
-                    
-                </li>
-            </ul>
+            <div class="col-8 pt-5 ">
+                <ul class="d-flex flex-wrap justify-content-center list-unstyled gap-5 mb-0 text-uppercase">
+                    <li v-for="currentLogo in store.logo">
+                        <img :src="currentLogo" alt="@" class="logo">
+                    </li>
+                </ul>
+            </div>
         </div>
         <hr class="m-0">
     </footer>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+li{
+    background: linear-gradient(0deg, #41485500 0%, rgba(113, 93, 128, 0.469) 100%);
+    backdrop-filter: blur(2px);
+    box-shadow: 0 7px 20px 5px #00000048;
+
+    border-radius: 10px;
+    
+    padding: 10px;
+
+    .logo{
+        width: 75px;
+        height: 75px;
+    }
+}
 
 </style>
--- src/store.js
+++ src/store.js
@@ -2,4 +2,17 @@ import { reactive } from "vue";
 
 export const store = reactive({
   projects: [],
+
+  logo: [
+    `../../public/html.png`,
+    '../../public/css.png',
+    '../../public/java-script.png',
+    '../../public/bootstrap.png',
+    '../../public/sass.png',
+    '../../public/vite.svg',
+    '../../public/vue.png',
+    '../../public/php.png',
+    '../../public/mysql.png',
+    '../../public/laravel.png',
+  ],
 });
