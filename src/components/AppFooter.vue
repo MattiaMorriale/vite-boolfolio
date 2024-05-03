145a3aaa6cc2a8dc138d7ca0bf4db54a49063777
mattia_morriale <mattia.morriale1@gmail.com>
2024-05-03 15:32:37 +0200

    add footer's icons

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -17,9 +17,27 @@ export default {
     <footer class="text-center p-3">
         <hr>
 
-        <span>©2024 Mattia Morriale, Pietro Antonio Nini, Vincenzo Sergi</span>
+        <div class="container px-5 py-3 d-flex ">
+            <div class="col-6 d-flex justify-content-start ">
+                <span>©2024 Mattia Morriale, Pietro Antonio Nini, Vincenzo Sergi</span>
+            </div>
+            <div class="col-6 d-flex justify-content-end gap-3">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-x-twitter"></i>
+                <i class="fa-brands fa-github"></i>
+                <i class="fa-brands fa-linkedin"></i>
+            </div>
+        </div>
+
     </footer>
 
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+i{
+    font-size: 20px;
+}
+
+</style>
